94174b1e21948ad8400fca51d294a229141d002c
Travis Desir <travisgdesir31@gmail.com>
2018-09-25 13:49:22 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
-  guess = 1+rand(6)
+  guess = rand(1..6)
   answer = gets.chomp.to_s
   if answer == guess
     puts "You guessed the correct number!"
