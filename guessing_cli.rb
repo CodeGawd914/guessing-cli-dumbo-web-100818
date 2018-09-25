9dbd005de2dc0595bbeae6b1ab88f3f35f13fdf1
Travis Desir <travisgdesir31@gmail.com>
2018-09-25 13:56:30 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,9 +1,11 @@
 def run_guessing_game
+  loop do
   puts "Guess a number between 1 and 6."
   guess = rand(1..6)
   answer = gets.chomp
   if answer == "exit" 
    puts "Goodbye!"
+   break
     elsif answer.to_i == guess 
     puts "You guessed the correct number!"
     elsif answer.to_i != guess
