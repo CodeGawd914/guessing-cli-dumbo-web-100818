d0fa9696f38a02bf6c67751cca4542e94d100322
Travis Desir <travisgdesir31@gmail.com>
2018-09-25 14:12:03 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,9 +9,29 @@ def run_guessing_game
       elsif answer.to_i == guess
           puts "You guessed the correct number!"
         elsif answer.to_i != guess
-          puts "The computer guessed #{guess}."
+          puts "The computer guessed #{answer}."
         else
           puts "Invalid input"
         end
       end
     end
+
+
+
+# def run_guessing_game
+#   loop do
+#       puts "Guess a number between 1 and 6."
+#       guess = gets.chomp
+#       computer_number = rand(1..6)
+#       if guess == "exit"
+#         puts "Goodbye!"
+#         break
+#       elsif guess.to_i == computer_number
+#           puts "You guessed the correct number!"
+#         elsif guess.to_i != computer_number
+#           puts "The computer guessed #{computer_number}."
+#         else
+#           puts "Invalid input"
+#         end
+#       end
+#     end
